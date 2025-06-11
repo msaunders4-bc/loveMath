57bfcb2125b1dc4cde9ff11376b1618336f34868
marc <msaunders4@hotmail.co.uk>
2025-06-11 14:50:31 +0100

    add js function structure

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+
+function runGame() {
+
+
+}
+
+function checkAnswer() {
+
+
+}
+
+function calculateCorrectAnswer() {
+
+
+}
+
+function incrementScore() {
+
+
+}
+
+function incremnentWrongAnswer() {
+
+
+}
+
+function additionQuestion() {
+
+
+}
+
+function subtractQuestion() {
+
+
+}
+
+function mutliplyQuestion() {
+
+
+}
+
+
